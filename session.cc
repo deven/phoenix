ce84b39710510065fe9428a1d8fa5ce482260fe5
Deven T. Corzine <deven@ties.org>
1993-12-11 18:42:42 -0500

    Add Session::Link() method.

--- session.cc
+++ session.cc
@@ -56,6 +56,12 @@ Session::~Session()
    delete user;
 }
 
+void Session::Link()		// Link session into global list.
+{
+   next = sessions;
+   sessions = this;
+}
+
 int Session::ResetIdle(int min) // Reset and return idle time, maybe report.
 {
    int now,idle,days,hours,minutes;
