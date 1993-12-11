a72e3eda2bfd021d370d579123d81e734413bf59
Deven T. Corzine <deven@ties.org>
1993-12-11 18:42:42 -0500

    Add Listen::Open() wrapper method.

--- listen.h
+++ listen.h
@@ -15,6 +15,7 @@ class Listen: public FD {
 private:
    void RequestShutdown(int port); // Try to shut down a running server.
 public:
+   static void Open(int port);	// Open a listening port.
    Listen(int port);		// constructor
    ~Listen() {			// destructor
       if (fd != -1) close(fd);
