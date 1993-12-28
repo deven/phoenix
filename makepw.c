cc756fb689794b2842133512fd304282f97fcef8
Deven T. Corzine <deven@ties.org>
1993-12-28 18:36:00 -0500

    Add "makepw" utility for encrypting passwords.

---
+++ makepw.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main(int argc,char **argv)
+{
+   char pw[9],salt[3];
+   char *key,*getpass();
+   key = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./";
+   sleep(1);
+   srandom(time(NULL));
+   salt[0] = key[random() & 63];
+   salt[1] = key[random() & 63];
+   salt[2] = 0;
+   if (argv[1]) {
+      printf("%s\n",crypt(argv[1],salt));
+      exit(0);
+   }
+   strcpy(pw,getpass("Enter password: "));
+   if (strcmp(pw,getpass("Re-enter password to verify: "))) exit(1);
+   printf("Encrypted password: %s\n",crypt(pw,salt));
+   exit(0);
+}
