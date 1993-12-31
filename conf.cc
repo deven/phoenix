9544c90f81fdeb13b72ee153653b43d782efe6f6
Deven T. Corzine <deven@ties.org>
1993-12-31 03:08:52 -0500

    Remove support for reversed smileys.
    Remove support for reversed smileys (e.g. "(-:") in case of match against
    a name, and because they're not as likely to be typed reflexively.

--- conf.cc
+++ conf.cc
@@ -146,9 +146,7 @@ char *message_start(char *line,char *sendlist,int len,boolean &explicit)
       if (!strcmp(line,":-)") || !strcmp(line,":-(") || !strcmp(line,":-P") ||
 	  !strcmp(line,";-)") || !strcmp(line,":_)") || !strcmp(line,":_(") ||
 	  !strcmp(line,":)") || !strcmp(line,":(") || !strcmp(line,":P") ||
-	  !strcmp(line,";)") || !strcmp(line,"(-:") || !strcmp(line,")-:") ||
-	  !strcmp(line,"(-;") || !strcmp(line,"(_:") || !strcmp(line,")_:") ||
-	  !strcmp(line,"(:") || !strcmp(line,"):") || !strcmp(line,"(;")) {
+	  !strcmp(line,";)")) {
 	 *p = state;
 	 strcpy(sendlist,"default");
 	 return line;
