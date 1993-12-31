cd41fe0644aedf46324f75e62a5d547e31d45943
Deven T. Corzine <deven@ties.org>
1993-12-31 03:08:52 -0500

    Don't print extra message for Session::DoReset().

--- session.cc
+++ session.cc
@@ -649,7 +649,7 @@ void Session::DoHelp()		// Do /help command.
 
 void Session::DoReset()		// Do <space><return> idle time reset.
 {
-   if (ResetIdle(1)) output("Your idle time has been reset.\n");
+   ResetIdle(1);
 }
 
 void Session::DoMessage(char *line) // Do message send.
