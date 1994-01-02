98d6c0b6d44fba72e9d410147aef91eb0c6978bc
Deven T. Corzine <deven@ties.org>
1994-01-02 06:26:40 -0500

    Use smart pointers to manage memory.

--- fdtable.h
+++ fdtable.h
@@ -28,7 +28,7 @@ class FDTable {			// File Descriptor Table
 private:
    static fd_set readfds;	// read fdset for select()
    static fd_set writefds;	// write fdset for select()
-   FD **array;
+   Pointer<FD> *array;
    int size;
    int used;
 public:
@@ -36,13 +36,13 @@ public:
    ~FDTable();			// destructor
    void OpenListen(int port);	// Open a listening port.
    void OpenTelnet(int lfd);	// Open a telnet connection.
-   FD *Closed(int fd);		// Close fd, return pointer to FD object.
+   Pointer<FD> Closed(int fd);	// Close fd, return pointer to FD object.
    void Close(int fd);		// Close fd, deleting FD object.
    void Select();		// Select across all ready connections.
    void InputReady(int fd);	// Input is ready on file descriptor fd.
    void OutputReady(int fd);	// Output is ready on file descriptor fd.
    void announce(char *buf);	// unformatted write to all connections
-   void nuke(Telnet *telnet,int fd,boolean drain);
+   void nuke(Pointer<Telnet> telnet,int fd,boolean drain);
    void ReadSelect(int fd) {	// Select fd for reading.
       FD_SET(fd,&readfds);
    }
