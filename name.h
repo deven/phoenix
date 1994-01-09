ae140e1a795a3d745fa3d903d6cd284a15dc876a
Deven T. Corzine <deven@ties.org>
1994-01-09 00:09:33 -0500

    Simplify usage of Pointers.
    Remove Null() construct for Pointers, and modify Pointer conversions.

--- name.h
+++ name.h
@@ -23,7 +23,7 @@ public:
 
    Name(Pointer<Session> s,Pointer<Name> &prev,char *str): session(s) {
       // Delete leading unused names. (may not work)
-      while (!prev.Null() && prev->References() == 1) prev = prev->next;
+      while (prev && prev->References() == 1) prev = prev->next;
       strncpy(name,str,NameLen); // Save name string.
       name[NameLen - 1] = 0;	 // Make sure name is terminated.
       next = prev;		 // Save pointer to previous used name.
