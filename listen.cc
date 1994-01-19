7e5ed2b103e99aa9abc21c031a432eeceb18caa7
Deven T. Corzine <deven@ties.org>
1994-01-19 16:50:32 -0500

    Clear close-on-exec flag for sockets.
    Clear close-on-exec flag for listen socket and all telnet connections.

--- listen.cc
+++ listen.cc
@@ -44,6 +44,7 @@ Listen::Listen(int port)	// Listen on a port.
    if ((fd = socket(AF_INET,SOCK_STREAM,0)) == -1) {
       error("Listen::Listen(): socket()");
    }
+   if (fcntl(fd,F_SETFD,0) == -1) error("Listen::Listen(): fcntl()");
    if (setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&option,sizeof(option))) {
       error("Listen::Listen(): setsockopt()");
    }
