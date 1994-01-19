0b5c7b3c4e126acf09d815ba7c48cbeabb3882b2
Deven T. Corzine <deven@ties.org>
1994-01-19 16:50:32 -0500

    Simplify Object class implementation.

--- object.h
+++ object.h
@@ -11,28 +11,15 @@
 // Initial revision
 //
 
+extern "C" volatile void abort();
+
 class Object {
 private:
    int RefCnt;			// Reference count.
 public:
-   Object() {			// Object constructor.
-      RefCnt = 0;		// Set reference count to zero.
-   }
-   ~Object() {			// Object destructor.
-      if (RefCnt) {		// Make sure there are no references left.
-	 void crash(char *format,...);
-	 crash("Object destroyed with %d outstanding references!",RefCnt);
-      }
-   }
-   int References() {		// Get reference count.
-      return RefCnt;
-   }
-   void NewReference() {	// Note a new reference to object.
-      RefCnt++;			// Increment reference count.
-   }
-   void DeleteReference() {	// Delete a reference to object.
-      if (--RefCnt == 0) {	// Decrement reference count.
-	 delete this;		// Delete object when last reference deleted.
-      }
-   }
+   Object(): RefCnt(0) { }
+   ~Object() { if (RefCnt) abort(); }
+   int References() { return RefCnt; }
+   void NewReference() { RefCnt++; }
+   void DeleteReference() { if (--RefCnt == 0) delete this; }
 };
