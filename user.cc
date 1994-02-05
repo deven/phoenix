39c3e98e26f9e049f80c18287db751fd4668162e
Deven T. Corzine <deven@ties.org>
1994-02-05 13:13:45 -0500

    Completely reorganize User class.
    Refactor User class to manage the password file, track sessions, etc.

--- user.cc
+++ user.cc
@@ -19,10 +19,52 @@
 
 #include "conf.h"
 
-User::User(Pointer<Session> &s): session(s) {
-   priv = 10;			// default user privilege level
-   strcpy(user,"[nobody]");	// Who is this?
-   password[0] = 0;		// No password.
-   reserved_name[0] = 0;	// No name.
-   default_blurb[0] = 0;	// No default blurb.
+List<User> User::users;
+
+User *User::GetUser(char *login)
+{
+   ListIter<User> u(users);
+   while (u++) if (!strcasecmp(u->user,login)) return u;
+   return NULL;
+}
+
+void User::Update(char *login,char *pass,char *name,char *defblurb,int p)
+{
+   User *u = GetUser(login);
+   if (!u) u = new User(login,pass,name,defblurb,p);
+   u->password = pass;
+   u->reserved = name;
+   u->blurb = defblurb;
+   u->priv = p;
+}
+
+void User::UpdateAll()		// Update all user entries from password file.
+{
+   char buf[256],*username,*password,*name,*priv,*p;
+   FILE *pw = fopen("passwd","r");
+   if (pw) {
+      while (fgets(buf,256,pw)) {
+	 if (buf[0] == '#') continue;
+	 p = username = buf;
+	 password = name = priv = 0;
+	 while (*p) if (*p==':') {*p++=0;password = p;break;} else p++;
+	 while (*p) if (*p==':') {*p++=0;name = p;break;} else p++;
+	 while (*p) if (*p==':') {*p++=0;priv = p;break;} else p++;
+	 if (!priv) continue;
+	 Update(username,password,name,NULL,atoi(priv ? priv : "0"));
+      }
+      fclose(pw);
+   }
+   Update("guest",NULL,NULL,NULL,0);
+}
+
+boolean User::CheckReserved(char *name)
+{
+   ListIter<User> u(users);
+   while (u++) {
+      if (u != this && u->reserved && !strcasecmp(u->reserved,name)) {
+	 return true;
+      }
+   }
+   return false;
 }
