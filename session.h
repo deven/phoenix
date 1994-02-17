fbdc224a3339386d332423cb6636531a0d5dda3c
Deven T. Corzine <deven@ties.org>
1994-02-17 00:15:56 -0500

    Add Session::PrintTimeLong() method.

--- session.h
+++ session.h
@@ -132,6 +132,7 @@ public:
    void ProcessInput(char *line);
    void NotifyEntry();		// Notify other users of entry and log.
    void NotifyExit();		// Notify other users of exit and log.
+   void PrintTimeLong(int minutes); // Print time value, long format.
    int ResetIdle(int min);	// Reset and return idle time, maybe report.
    void SetIdle(char *args);	// Set idle time.
    void SetBlurb(char *newblurb); // Set a new blurb.
