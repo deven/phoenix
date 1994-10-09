d7f1a7021b8d5fbb558de040c710ad80b9ac3edf
Deven T. Corzine <deven@ties.org>
1994-10-09 18:51:25 -0400

    RCS checkin (4 files)
    Makefile
    assoc.h
    assoc.cc session.cc

--- session.cc
+++ session.cc
@@ -1,12 +1,15 @@
 // -*- C++ -*-
 //
-// $Id: session.cc,v 1.30 1994/08/22 07:14:52 deven Exp $
+// $Id: session.cc,v 1.31 1994/10/09 22:49:50 deven Exp $
 //
 // Session class implementation.
 //
 // Copyright 1994 by Deven T. Corzine.  All rights reserved.
 //
 // $Log: session.cc,v $
+// Revision 1.31  1994/10/09 22:49:50  deven
+// Fixed bug in "/who guests".
+//
 // Revision 1.30  1994/08/22 07:14:52  deven
 // Moved trim(line) to after message_start() at least.
 //
