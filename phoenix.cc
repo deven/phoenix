ad6e5f116676dbea6054e5f16167a313a2571a34
Deven T. Corzine <deven@ties.org>
1995-12-05 15:14:12 -0500

    Improve handling of system uptime.
    Add SystemUptime() function (reads /proc/uptime if available) and
    ServerStartUptime variable to hold the system uptime at the time of
    server start, for more reliable determination of server uptime.

--- phoenix.cc
+++ phoenix.cc
@@ -16,6 +16,7 @@ int Shutdown;			// shutdown flag ***
 FILE *logfile;			// log file ***
 
 time_t ServerStartTime;		// time server started
+int ServerStartUptime;		// system uptime when server started
 
 // class Date? ***
 char *date(time_t clock,int start,int len) // get part of date string ***
@@ -163,6 +164,18 @@ void ShutdownServer()		// Shutdown server.
    exit(0);
 }
 
+int SystemUptime()		// Get system uptime, if available.
+{
+   int uptime = 0;
+   FILE *fp = fopen("/proc/uptime","r");
+
+   if (fp) {
+      fscanf(fp,"%d",&uptime);
+      fclose(fp);
+   }
+   return uptime;
+}
+
 void trim(char *&input) {
    while (*input && isspace(*input)) input++;
    char *p = input;
@@ -203,7 +216,10 @@ int main(int argc,char **argv)	// main program
    int pid;			// server process number
    int port;			// TCP port to use
 
-   time(&ServerStartTime);	// Mark time server started.
+   // Mark server start with current time and system uptime if available.
+   time(&ServerStartTime);
+   ServerStartUptime = SystemUptime();
+
    Shutdown = 0;
    if (pw = getpwuid(geteuid())) {
       home = pw->pw_dir;
