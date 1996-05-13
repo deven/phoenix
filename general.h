3b3260c2791021621f6c9ac1b7ad826d7fdb5d1d
Deven T. Corzine <deven@ties.org>
1996-05-13 14:17:56 -0400

    Move global variables to new globals.h header.

--- general.h
+++ general.h
@@ -97,14 +97,3 @@ class Session;
 class Telnet;
 class Timestamp;
 class User;
-
-extern int errno;		// System error number
-extern char *sys_errlist[];	// System error list
-extern int sys_nerr;		// Size of system error list
-
-extern FILE *logfile;		// log file ***
-
-extern int Shutdown;		// shutdown flag
-
-extern Timestamp ServerStartTime; // time server started
-extern int ServerStartUptime;	// system uptime when server started
