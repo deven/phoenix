e3689044461211ba3f4659c86fe118013969f6ef
Deven T. Corzine <deven@ties.org>
2001-12-04 12:56:00 -0500

    Add additional String operations for completeness.
    Add additional operations for completeness, including const/non-const
    variants.

--- string.h
+++ string.h
@@ -36,29 +36,46 @@ public:
       str[len] = 0;
    }
    String(const String &s);
+   String(String &s);
    String(const char *s);
+   String(char *s);
    String(const char *s, int n);
+   String(char *s, int n);
    String(int n);
    String(unsigned int n);
    String(long n);
    String(unsigned long n);
    ~String() { delete [] str; }
    String &operator =(const String &s);
+   String &operator =(String &s);
    String &operator =(const char *s);
+   String &operator =(char *s) { return operator =((const char *) s); }
    String &append(const String &s);
+   String &append(String &s);
    String &append(const char *s);
+   String &append(char *s) { return append((const char *) s); }
    String &append(char c);
    String &prepend(const String &s);
+   String &prepend(String &s);
    String &prepend(const char *s);
+   String &prepend(char *s) { return prepend((const char *) s); }
    String &prepend(char c);
    int operator ==(const String &s) {
       return len == s.len && !strncmp(str, s.str, len);
    }
+   int operator ==(String &s) {
+      return len == s.len && !strncmp(str, s.str, len);
+   }
    int operator ==(const char *s) { return !strcmp(str, s ? s : ""); }
+   int operator ==(char *s) { return !strcmp(str, s ? s : ""); }
    int operator !=(const String &s) {
       return len != s.len || strncmp(str, s.str, len) != 0;
    }
+   int operator !=(String &s) {
+      return len != s.len || strncmp(str, s.str, len) != 0;
+   }
    int operator !=(const char *s) { return strcmp(str, s ? s : "") != 0; }
+   int operator !=(char *s) { return strcmp(str, s ? s : "") != 0; }
    const char *operator ~() const { return str; }
    char *operator ~() { return str; }
    operator const char *() const { return str; }
@@ -74,6 +91,9 @@ public:
 class StringObj: public Object, public String {
 public:
    StringObj(const String &s): Object(), String(s) { }
+   StringObj(String &s): Object(), String(s) { }
    StringObj(const char *s): Object(), String(s) { }
+   StringObj(char *s): Object(), String(s) { }
    StringObj(const char *s, int n): Object(), String(s, n) { }
+   StringObj(char *s, int n): Object(), String(s, n) { }
 };
