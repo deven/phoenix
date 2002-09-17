3924cdc5b601e7436a5de63e7d60e3940f6d47af
Deven T. Corzine <deven@ties.org>
2002-09-17 00:49:50 -0400

    Use configure to test for built-in C++ "bool" type.

--- boolean.h
+++ boolean.h
@@ -23,20 +23,9 @@
 // Initial revision
 //
 
-// GCC versions beyond 2.5.8 have builtin "bool" boolean data type.
-#if defined(__GNUC__) && (__GNUC__ > 2 || __GNUC__ == 2 && __GNUC_MINOR__ > 5)
-#define BOOL_TYPE 1
-#endif
-
 // boolean type
-#ifdef NO_BOOLEAN
-#define boolean int
-#define false (0)
-#define true (1)
-#else
-#ifdef BOOL_TYPE
+#ifdef HAVE_BOOL
 typedef bool boolean;		// builtin boolean data type
 #else
 enum boolean { false, true };	// boolean data type
-#endif
 #endif
