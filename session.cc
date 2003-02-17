fd6d868d1876c5bc4dd9303ed2026637187562f1
Deven T. Corzine <deven@ties.org>
2003-02-17 01:57:53 -0500

    CVS checkin (1 file)
    session.cc

--- session.cc
+++ session.cc
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 //
-// $Id: session.cc,v 1.7 2003/02/17 06:40:06 deven Exp $
+// $Id: session.cc,v 1.8 2003/02/17 06:57:53 deven Exp $
 //
 // Session class implementation.
 //
@@ -19,6 +19,9 @@
 // for more information or if any conditions of this licensing are unclear.
 //
 // $Log: session.cc,v $
+// Revision 1.8  2003/02/17 06:57:53  deven
+// Forgot to fix the comparison when reducing the display of discussion titles!
+//
 // Revision 1.7  2003/02/17 06:40:06  deven
 // Modified to use String::vsprintf() and String::sprintf() in preference to
 // the system vsprintf() and sprintf() functions, to avoid buffer overflows.
