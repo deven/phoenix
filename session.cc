6daf87ce0dafff9ec233eb22f4d4c477b85ea778
Deven T. Corzine <deven@ties.org>
2003-02-21 23:39:59 -0500

    CVS checkin (3 files)
    session.h
    session.cc telnet.cc

--- session.cc
+++ session.cc
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 //
-// $Id: session.cc,v 1.14 2003/02/22 04:36:40 deven Exp $
+// $Id: session.cc,v 1.15 2003/02/22 04:39:59 deven Exp $
 //
 // Session class implementation.
 //
@@ -19,6 +19,9 @@
 // for more information or if any conditions of this licensing are unclear.
 //
 // $Log: session.cc,v $
+// Revision 1.15  2003/02/22 04:39:59  deven
+// Modified Session::SetInputFunction() to set prompt with input function.
+//
 // Revision 1.14  2003/02/22 04:36:40  deven
 // Modified to properly close initializing session after a session transfer.
 //
