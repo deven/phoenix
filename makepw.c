e6d8ac51fda80a497df6bbb5349a8a4527dda711
Deven T. Corzine <deven@ties.org>
2021-07-22 17:31:21 -0400

    Rename Gangplank back to Phoenix.
    Rename the Gangplank conferencing system back to "Phoenix", which was
    the original name used before the original Open Source release on
    November 30, 2001.

--- makepw.c
+++ makepw.c
@@ -6,7 +6,7 @@
  *
  * Copyright 1992-1996, 2000-2003 by Deven T. Corzine.  All rights reserved.
  *
- * This file is part of the Gangplank conferencing system.
+ * This file is part of the Phoenix conferencing system.
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined by Trolltech AS of Norway (except for Choice of Law) and as
@@ -15,7 +15,7 @@
  * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
  * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * Visit <http://www.gangplank.org/license/> or contact <info@gangplank.org>
+ * Visit <http://www.phoenix-cmc.org/license/> or contact <info@phoenix-cmc.org>
  * for more information or if any conditions of this licensing are unclear.
  *
  */
