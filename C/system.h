e380dcfa0bd55eda4b8bd490be4b1c35f128d72e
Deven T. Corzine <deven@ties.org>
2021-07-28 16:32:25 -0400

    Add "system.h" include file.

---
+++ C/system.h
@@ -0,0 +1,101 @@
+/* -*- C -*-
+ *
+ * $Id$
+ *
+ * System include files.
+ *
+ * Copyright 1992-2021 Deven T. Corzine <deven@ties.org>
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#define HAVE_ARPA_INET_H
+#define HAVE_CRYPT_H
+#define HAVE_FCNTL_H
+#define HAVE_MEMORY_H
+#define HAVE_NETDB_H
+#define HAVE_NETINET_IN_H
+#define HAVE_STRINGS_H
+#define HAVE_SYS_IOCTL_H
+#define HAVE_SYS_SELECT_H
+#define HAVE_SYS_SOCKET_H
+#define HAVE_SYS_STAT_H
+#define HAVE_SYS_TIME_H
+#define HAVE_SYS_TYPES_H
+#define HAVE_SYS_WAIT_H
+#define HAVE_UNISTD_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <signal.h>
+#include <pwd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+
+#ifdef HAVE_MEMORY_H
+#include <memory.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef HAVE_FCNTL_H
+#include <fcntl.h>
+#endif
+
+#ifdef HAVE_NETDB_H
+#include <netdb.h>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
+
+#ifdef HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+
+#ifdef HAVE_SYS_IOCTL_H
+#include <sys/ioctl.h>
+#endif
+
+#ifdef HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif
+
+#ifdef HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#else
+#define WEXITSTATUS(status)   (((status) & 0xff00) >> 8)
+#define WIFEXITED(status)     (((status) & 0x7f) == 0)
+#endif
+
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+
+#ifdef HAVE_ARPA_INET_H
+#include <arpa/inet.h>
+#endif
+
+#ifdef HAVE_CRYPT_H
+#include <crypt.h>
+#endif
+
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h>
+#endif
