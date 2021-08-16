cefe1c16677bc48de9fae09f0cadedcdfed46280
Deven T. Corzine <deven@ties.org>
2021-08-15 23:37:01 -0400

    Add example echo server code from tokio docs.

--- phoenix_cmc/src/main.rs
+++ phoenix_cmc/src/main.rs
@@ -9,8 +9,10 @@
 // SPDX-License-Identifier: MIT
 //
 
-use std::net::{IpAddr, Ipv6Addr, SocketAddr, TcpListener};
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 use structopt::StructOpt;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[derive(Debug, StructOpt)]
 struct Opts {
@@ -31,9 +33,36 @@ struct Opts {
     port: u16,
 }
 
-fn main() {
-    let opts      = Opts::from_args();
-    let socket    = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), opts.port);
-    let _listener = TcpListener::bind(socket).unwrap();
-    println!("opts: {:?}", opts);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opts     = Opts::from_args();
+    let socket   = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), opts.port);
+    let listener = TcpListener::bind(socket).await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
 }
