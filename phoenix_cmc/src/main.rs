1249dc55ba34253d3c869c633cacf58af9fc813b
Deven T. Corzine <deven@ties.org>
2021-08-15 23:17:50 -0400

    Add TcpListener code.

--- phoenix_cmc/src/main.rs
+++ phoenix_cmc/src/main.rs
@@ -9,6 +9,7 @@
 // SPDX-License-Identifier: MIT
 //
 
+use std::net::{IpAddr, Ipv6Addr, SocketAddr, TcpListener};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -21,12 +22,18 @@ struct Opts {
     #[structopt(long)]
     debug: bool,
 
+    /// Use IPv6 instead of IPv4
+    #[structopt(long)]
+    ipv6: bool,
+
     /// Set listening port number
     #[structopt(long, default_value = "9999")]
     port: u16,
 }
 
 fn main() {
-    let opts = Opts::from_args();
+    let opts      = Opts::from_args();
+    let socket    = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), opts.port);
+    let _listener = TcpListener::bind(socket).unwrap();
     println!("opts: {:?}", opts);
 }
