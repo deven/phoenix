2e10695c65dbad48834d78aa1fd360510a1f04b5
Deven T. Corzine <deven@ties.org>
2021-08-15 21:05:45 -0400

    Add comment header.

--- phoenix_cmc/src/main.rs
+++ phoenix_cmc/src/main.rs
@@ -1,3 +1,14 @@
+// -*- Rust -*-
+//
+// $Id$
+//
+// Main program.
+//
+// Copyright 2021 Deven T. Corzine <deven@ties.org>
+//
+// SPDX-License-Identifier: MIT
+//
+
 fn main() {
     println!("Hello, world!");
 }
