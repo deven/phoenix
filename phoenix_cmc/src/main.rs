f4a1fba04e24672d8cec4c5073a4090d822c5e37
Deven T. Corzine <deven@ties.org>
2021-08-15 21:07:50 -0400

    Add Opts struct for command-line arguments.

--- phoenix_cmc/src/main.rs
+++ phoenix_cmc/src/main.rs
@@ -9,6 +9,23 @@
 // SPDX-License-Identifier: MIT
 //
 
+#[derive(Debug)]
+struct Opts {
+    /// Run from cron to restart server
+    cron: bool,
+
+    /// Enable debug mode
+    debug: bool,
+
+    /// Set listening port number
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts {
+        cron:  false,
+        debug: false,
+        port:  9999,
+    };
+    println!("opts: {:?}", opts);
 }
