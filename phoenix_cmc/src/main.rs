69e7fdfbfc6696add0e37115908c5bfd505001fc
Deven T. Corzine <deven@ties.org>
2021-08-20 03:56:53 -0400

    Add some tracing events.

--- phoenix_cmc/src/main.rs
+++ phoenix_cmc/src/main.rs
@@ -14,6 +14,7 @@ use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 use structopt::StructOpt;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
+use tracing::{warn, info};
 use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
 #[derive(Debug, StructOpt)]
@@ -46,11 +47,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let socket   = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), opts.port);
     let listener = TcpListener::bind(socket).await?;
 
+    info!("Phoenix CMC running, accepting connections on port {}.", opts.port);
+
     loop {
         let (mut socket, _) = listener.accept().await?;
 
+        info!("Accepted connection from {:?}", socket);
+
         tokio::spawn(async move {
             let mut buf = [0; 1024];
+            info!("Spawned a new async task.");
 
             // In a loop, read data from the socket and write the data back.
             loop {
@@ -59,14 +65,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     Ok(n) if n == 0 => return,
                     Ok(n) => n,
                     Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
+                        warn!("failed to read from socket; err = {:?}", e);
                         return;
                     }
                 };
 
                 // Write the data back
                 if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
+                    warn!("failed to write to socket; err = {:?}", e);
                     return;
                 }
             }
