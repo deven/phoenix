8aba8feac685d222e395ca8fde92e92518d99970
Deven T. Corzine <deven@ties.org>
2023-01-13 04:44:55 -0500

    Create separate "client" module.

--- phoenix_cmc/src/lib.rs
+++ phoenix_cmc/src/lib.rs
@@ -11,22 +11,19 @@
 
 #![warn(rust_2018_idioms)]
 
-use async_backtrace::{frame, framed, taskdump_tree};
+mod client;
+
+use crate::client::{setup_client, Client};
+use async_backtrace::frame;
 use clap::Parser;
-use futures::SinkExt;
 use std::collections::HashMap;
 use std::error::Error;
-use std::io;
 use std::io::ErrorKind;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 use std::sync::Arc;
-use tokio::io::AsyncWriteExt;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+use tokio::net::TcpListener;
 use tokio::sync::Mutex;
-use tokio_stream::StreamExt;
-use tokio_util::codec::{Framed, LinesCodec};
-use tracing::{error, info, trace, warn};
+use tracing::{error, info, warn};
 use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
 #[derive(Debug, Parser)]
@@ -49,7 +46,7 @@ pub struct Options {
 }
 
 /// Shared state between async tasks.
-struct SharedState {
+pub struct SharedState {
     clients: HashMap<SocketAddr, Arc<Mutex<Client>>>,
 }
 
@@ -59,42 +56,6 @@ impl SharedState {
         SharedState {
             clients: HashMap::new(),
         }
-    }
-}
-
-struct Client {
-    username: String,
-    addr: SocketAddr,
-    lines: Framed<TcpStream, LinesCodec>,
-    sender: UnboundedSender<String>,
-    receiver: UnboundedReceiver<String>,
-}
-
-impl Client {
-    /// Create a new instance of `Client`.
-    async fn new(
-        username: String,
-        addr: SocketAddr,
-        lines: Framed<TcpStream, LinesCodec>,
-        state: Arc<Mutex<SharedState>>,
-    ) -> io::Result<Arc<Mutex<Client>>> {
-        // Create a channel for sending events to this client.
-        let (sender, receiver) = unbounded_channel();
-
-        // Create the new `Client` instance.
-        let client = Arc::new(Mutex::new(Client {
-            username,
-            addr,
-            lines,
-            sender,
-            receiver,
-        }));
-
-        // Save the new instance in the server shared state HashMap.
-        state.lock().await.clients.insert(addr, client.clone());
-
-        // Return the new instance.
-        Ok(client)
     }
 }
 
@@ -140,57 +101,5 @@ pub async fn run(options: Options) -> Result<(), Box<dyn Error>> {
             }
             Err(e) => warn!("Error accepting TCP connection: {:?}", e),
         }
-    }
-}
-
-/// Setup a new client connection.
-#[framed]
-async fn setup_client(
-    addr: SocketAddr,
-    stream: TcpStream,
-    state: Arc<Mutex<SharedState>>,
-) -> Result<(), Box<dyn Error>> {
-    let mut lines = Framed::new(stream, LinesCodec::new());
-
-    {
-        let stream = lines.get_mut();
-        stream.write_all(b"Enter username: ").await?;
-    }
-
-    let username = match lines.next().await {
-        Some(Ok(line)) => line,
-        _ => {
-            info!(
-                "Client disconnected from {} without sending a username.",
-                addr
-            );
-            return Ok(());
-        }
-    };
-
-    info!("User \"{}\" logged in from {}.", username, addr);
-
-    client_loop(&mut lines, state).await?;
-
-    info!("User \"{}\" disconnected from {}.", username, addr);
-
-    Ok(())
-}
-
-/// Client main loop.
-#[framed]
-async fn client_loop(
-    lines: &mut Framed<TcpStream, LinesCodec>,
-    state: Arc<Mutex<SharedState>>,
-) -> Result<(), Box<dyn Error>> {
-    trace!("{}", taskdump_tree(false));
-
-    // In a loop, read lines from the socket and write them back.
-    loop {
-        let input = match lines.next().await {
-            Some(Ok(line)) => line,
-            _ => return Ok(()),
-        };
-        lines.send(input).await?;
     }
 }
