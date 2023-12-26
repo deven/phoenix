c1faec005f4580ea0456c0df4824e119306f8e7b
Deven T. Corzine <deven@ties.org>
2023-12-26 03:27:40 -0500

    Add SessionTxError/SessionRxError to PhoenixError enum.

--- src/error.rs
+++ src/error.rs
@@ -9,24 +9,28 @@
 // SPDX-License-Identifier: MIT
 //
 
+use crate::client::session::SessionMessage;
 use std::error::Error;
 use std::fmt;
 use std::path::PathBuf;
+use tokio::sync::{mpsc, oneshot};
 use tracing::{trace, warn};
 
 #[derive(Debug)]
 pub enum PhoenixError {
-    //RequestError(reqwest::Error),
     FileIoError {
         path: PathBuf,
         source: std::io::Error,
     },
+    SessionTxError(mpsc::error::SendError<SessionMessage>),
+    SessionRxError(oneshot::error::RecvError),
 }
 
 impl Error for PhoenixError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            //PhoenixError::RequestError(err) => Some(err),
+            PhoenixError::SessionTxError(err) => Some(err),
+            PhoenixError::SessionRxError(err) => Some(err),
             PhoenixError::FileIoError { source, .. } => Some(source),
         }
     }
@@ -35,7 +39,8 @@ impl Error for PhoenixError {
 impl fmt::Display for PhoenixError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            //PhoenixError::RequestError(err) => write!(f, "Request error: {}", err),
+            PhoenixError::SessionTxError(err) => write!(f, "Session Tx error: {}", err),
+            PhoenixError::SessionRxError(err) => write!(f, "Session Rx error: {}", err),
             PhoenixError::FileIoError { path, source } => {
                 write!(f, "File I/O error for path {}: {}", path.display(), source)
             }
@@ -43,8 +48,14 @@ impl fmt::Display for PhoenixError {
     }
 }
 
-//impl From<reqwest::Error> for PhoenixError {
-//    fn from(error: reqwest::Error) -> Self {
-//        PhoenixError::RequestError(error)
-//    }
-//}
+impl From<mpsc::error::SendError<SessionMessage>> for PhoenixError {
+    fn from(error: mpsc::error::SendError<SessionMessage>) -> Self {
+        PhoenixError::SessionTxError(error)
+    }
+}
+
+impl From<oneshot::error::RecvError> for PhoenixError {
+    fn from(error: oneshot::error::RecvError) -> Self {
+        PhoenixError::SessionRxError(error)
+    }
+}
