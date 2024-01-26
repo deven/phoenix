a55439d28f6b3018a298e2289ac16a88a1a00043
Deven T. Corzine <deven@intellitree.com>
2024-01-26 06:36:50 -0500

    Reimplement attribute getters/setters with macros.
    This is almost working, except the Into<T> case.

--- src/event.rs
+++ src/event.rs
@@ -31,6 +31,47 @@ pub enum Event {
     LoginTimeout { client: Client },
 }
 
+macro_rules! attr {
+    ($getter:ident, $setter:ident, $type:ty, Into, [$($variant:ident),*]) => {
+        attr_impl!($getter, $setter, $type, ($getter.clone()), $setter<T: Into<$type>>, T, ($setter.into()), [$($variant),*]);
+    };
+    ($getter:ident, $setter:ident, $type:ty, Clone, [$($variant:ident),*]) => {
+        attr_impl!($getter, $setter, $type, ($getter.clone()), $setter, $type, ($setter), [$($variant),*]);
+    };
+    ($getter:ident, $setter:ident, $type:ty, Copy, [$($variant:ident),*]) => {
+        attr_impl!($getter, $setter, $type, (*$getter), $setter, $type, ($setter), [$($variant),*]);
+    };
+}
+
+macro_rules! attr_impl {
+    ($getter:ident, $setter:ident, $type:ty, $clone:tt, $decl:tt, $type2:tt, $into:tt, [$($variant:ident),*]) => {
+        #[framed]
+        pub async fn $getter(&self) -> Result<$type, EventError> {
+            let event = self.read().await;
+            match &*event {
+                $(
+                    Event::$variant { $getter, .. } => Ok($clone),
+                )*
+                _ => Err(EventError::invalid_getter(stringify!($getter), self.clone())),
+            }
+        }
+
+        #[framed]
+        pub async fn $decl(&self, $setter: $type2) -> Result<(), EventError> {
+            let mut event = self.write().await;
+            match *event {
+                $(
+                    Event::$variant { ref mut $getter, .. } => {
+                        *$getter = $into;
+                        Ok(())
+                    }
+                ),*
+                _ => Err(EventError::invalid_setter(stringify!($setter), self.clone())),
+            }
+        }
+    };
+}
+
 impl EventRef {
     /// Create a new message event.
     pub fn new_message<T: Into<Arc<str>>>(sender: Session, message: T) -> Self {
@@ -83,135 +124,32 @@ impl EventRef {
         self.0.write().await
     }
 
-    #[framed]
-    pub async fn client(&self) -> Result<Client, EventError> {
-        let event = self.read().await;
-        match &*event {
-            Event::LoginTimeout { client } => Ok(client.clone()),
-            _ => Err(EventError::new_invalid_getter("client", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn set_client(&self, new_client: Client) -> Result<(), EventError> {
-        let mut event = self.write().await;
-        match *event {
-            Event::LoginTimeout { ref mut client } => {
-                *client = new_client;
-                Ok(())
-            }
-            _ => Err(EventError::new_invalid_setter("client", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn message(&self) -> Result<Arc<str>, EventError> {
-        let event = self.read().await;
-        match &*event {
-            Event::Message { message, .. } => Ok(message.clone()),
-            _ => Err(EventError::new_invalid_getter("message", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn set_message<T: Into<Arc<str>>>(&self, new_message: T) -> Result<(), EventError> {
-        let mut event = self.write().await;
-        match *event {
-            Event::Message { ref mut message, .. } => {
-                *message = new_message.into();
-                Ok(())
-            }
-            _ => Err(EventError::new_invalid_setter("message", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn name(&self) -> Result<Arc<str>, EventError> {
-        let event = self.read().await;
-        match &*event {
-            Event::EntryNotify { name } => Ok(name.clone()),
-            Event::ExitNotify { name } => Ok(name.clone()),
-            _ => Err(EventError::new_invalid_getter("name", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn set_name<T: Into<Arc<str>>>(&self, new_name: T) -> Result<(), EventError> {
-        let mut event = self.write().await;
-        match *event {
-            Event::EntryNotify { ref mut name } => {
-                *name = new_name.into();
-                Ok(())
-            }
-            Event::ExitNotify { ref mut name } => {
-                *name = new_name.into();
-                Ok(())
-            }
-            _ => Err(EventError::new_invalid_setter("name", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn seconds(&self) -> Result<u16, EventError> {
-        let event = self.read().await;
-        match &*event {
-            Event::Shutdown { seconds } => Ok(*seconds),
-            Event::Restart { seconds } => Ok(*seconds),
-            _ => Err(EventError::new_invalid_getter("seconds", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn set_seconds(&self, new_seconds: u16) -> Result<(), EventError> {
-        let mut event = self.write().await;
-        match *event {
-            Event::Shutdown { ref mut seconds } => {
-                *seconds = new_seconds;
-                Ok(())
-            }
-            Event::Restart { ref mut seconds } => {
-                *seconds = new_seconds;
-                Ok(())
-            }
-            _ => Err(EventError::new_invalid_setter("seconds", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn sender(&self) -> Result<Session, EventError> {
-        let event = self.read().await;
-        match &*event {
-            Event::Message { sender, .. } => Ok(sender.clone()),
-            _ => Err(EventError::new_invalid_getter("sender", self.clone())),
-        }
-    }
-
-    #[framed]
-    pub async fn set_sender(&self, new_sender: Session) -> Result<(), EventError> {
-        let mut event = self.write().await;
-        match *event {
-            Event::Message { ref mut sender, .. } => {
-                *sender = new_sender;
-                Ok(())
-            }
-            _ => Err(EventError::new_invalid_setter("sender", self.clone())),
-        }
-    }
+    attr!(client, set_client, Client, Clone, [LoginTimeout]);
+    //attr!(message, set_message, Arc<str>, Into, [Message]);
+    //attr!(name, set_name, Arc<str>, Into, [EntryNotify, ExitNotify]);
+    attr!(seconds, set_seconds, u16, Copy, [Shutdown, Restart]);
+    attr!(sender, set_sender, Session, Clone, [Message]);
 }
 
 #[derive(Debug)]
 pub enum EventError {
-    InvalidGetter { attr: &'static str, event: EventRef },
-    InvalidSetter { attr: &'static str, event: EventRef },
+    InvalidGetter {
+        getter: &'static str,
+        event: EventRef,
+    },
+    InvalidSetter {
+        setter: &'static str,
+        event: EventRef,
+    },
 }
 
 impl EventError {
-    pub fn new_invalid_getter(attr: &'static str, event: EventRef) -> Self {
-        Self::InvalidGetter { attr, event }
+    pub fn invalid_getter(getter: &'static str, event: EventRef) -> Self {
+        Self::InvalidGetter { getter, event }
     }
 
-    pub fn new_invalid_setter(attr: &'static str, event: EventRef) -> Self {
-        Self::InvalidSetter { attr, event }
+    pub fn invalid_setter(setter: &'static str, event: EventRef) -> Self {
+        Self::InvalidSetter { setter, event }
     }
 }
 
@@ -223,15 +161,10 @@ impl Error for EventError {
 
 impl fmt::Display for EventError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let called = "() called on invalid event variant: ";
         match self {
-            Self::InvalidGetter { attr, event } => write!(
-                f,
-                "Getter {attr}() called on invalid event variant: {event:#?}"
-            ),
-            Self::InvalidSetter { attr, event } => write!(
-                f,
-                "Setter set_{attr}() called on invalid event variant: {event:#?}"
-            ),
+            Self::InvalidGetter { getter, event } => write!(f, "Getter {getter}{called}{event:#?}"),
+            Self::InvalidSetter { setter, event } => write!(f, "Setter {setter}{called}{event:#?}"),
         }
     }
 }
